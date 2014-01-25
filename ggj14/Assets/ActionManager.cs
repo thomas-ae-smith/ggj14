7bf8da34844648d1b82e5c4c88464a197189cafc
thomas ae smith <thomas.ae.smith@googlemail.com>
2014-01-25 23:55:43 +0000

    Assets.

---
+++ ggj14/Assets/ActionManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionManager : MonoBehaviour {
+
+	public string prompt;
+	public Sprite evil;
+	public int cost, payback;
+	private GameObject image;
+
+
+	// Use this for initialization
+	void Start () {
+		image = new GameObject("Reflection");
+		image.transform.parent = this.transform;
+		Vector3 pos = this.transform.position;
+		pos.y = -pos.y;
+		image.transform.position = pos;
+		Vector3 theScale = image.transform.localScale;
+		theScale.y *= -1;
+		image.transform.localScale = theScale;
+		//		image.transform.position.y = -image.transform.position.y;
+		image.AddComponent<SpriteRenderer> ();
+		image.GetComponent<SpriteRenderer> ().sprite = evil;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
